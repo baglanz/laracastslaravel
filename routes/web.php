0569196fada0c83b2ed06e5c392fbb94863f43fd
baglanz <baglan.zhaksylyk98@gmail.com>
2024-05-10 12:35:51 +0500

    Eager Loading and the N+1 Problem - Ep 13

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $jobs = Job::all();
 
-    dd($jobs);
-//    return view('home');
+    return view('home');
 });
 
 Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->get();
+
     return view('jobs', [
-        'jobs' => Job::all()
+        'jobs' => $jobs,
     ]);
 });
 
