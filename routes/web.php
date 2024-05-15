86bf9690ea95ca9a8845c4f7b64de62f8d8c98cd
baglanz <baglan.zhaksylyk98@gmail.com>
2024-05-15 17:24:07 +0500

    Editing, Updating, and Deleting a Resource - Ep 18

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,10 @@ Route::get('/jobs/{id}', function ($id) {
 
 Route::post('/jobs', function () {
     request()->validate([
-        'title' => ['required', 'min:3']
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
     ]);
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
@@ -40,6 +42,33 @@ Route::post('/jobs', function () {
     return redirect('/jobs');
 });
 
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+Route::delete('/jobs/{id}', function ($id) {
+    Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
+});
 
 Route::get('/contact', function () {
     return view('contact');
