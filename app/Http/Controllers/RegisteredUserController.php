751bd61a12263785b4273c91adb93786d3974b03
baglanz <baglan.zhaksylyk98@gmail.com>
2024-05-19 16:12:36 +0500

    Make a Login and Registration System From Scratch: Part 2 - Ep 22

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +15,16 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd('Store');
+        $attributes = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', 'min:8'],
+        ]);
+
+        $user = User::create($attributes);
+
+        Auth::login($user);
+
+        return redirect('/jobs');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                     <x-form-field>
                         <x-form-label for="email">Email</x-form-label>
                         <div class="mt-2">
-                            <x-form-input type="email" name="email" id="email" placeholder="bob@example.com" required />
+                            <x-form-input type="email" name="email" id="email" :value="old('email')" placeholder="bob@example.com" required />
                             <x-form-error name="email" />
                         </div>
                     </x-form-field>
